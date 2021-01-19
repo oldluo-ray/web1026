89ad389adf6ebd408ef75632913be8486ba73f95
oldluo-ray <luodianlei7@163.com>
2021-01-19 11:50:37 +0800

    解决跨域

--- day07-code/01-ajax/server.js
+++ day07-code/01-ajax/server.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 app.use(express.static('public'))
 app.get('/test', (req, res) => {
-  setTimeout(() => {
-    res.send('响应')
-  }, 1000)
+  const { callback } = req.query
+  res.send(`${callback}({"name": "zs", "age":18})`) // res.send(fn({"name": "zs", "age":18}))
+})
+
+app.post('/test', (req, res) => {
+  res.set('Access-Control-Allow-Origin', '*')
+  res.send('{"name":"zs", "age": 18}')
 })
 app.listen(5000)

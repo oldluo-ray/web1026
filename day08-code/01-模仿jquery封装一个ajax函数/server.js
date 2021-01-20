089d2dede893fd1be0843981b235578fbb68a0a4
oldluo-ray <luodianlei7@163.com>
2021-01-20 11:51:40 +0800

    配合promise

--- day08-code/01-模仿jquery封装一个ajax函数/server.js
+++ day08-code/01-模仿jquery封装一个ajax函数/server.js
@@ -1,11 +1,19 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-  console.log(req.url)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('content-type', 'text/html; charset=utf8')
-  res.end('后台响应的数据')
+const express = require('express')
+// const cors = require('cors')
+const app = express()
+// app.use(cors())
+// app.use(express.urlencoded({ extended: true })) //键=值&键=值
+// app.use(express.json()) // 将客户端传过来的json格式的数据,解析之后,添加到req.body上面
+app.use((req, res, next) => {
+  res.set('access-control-allow-origin', '*')
+  next()
 })
-server.listen(5000, (err) => {
-  if (err) console.log(err)
-  else console.log('启动成功')
+app.get('/test', (req, res) => {
+  res.send(req.query)
 })
+
+app.post('/test', (req, res) => {
+  res.send(req.body)
+})
+
+app.listen(5000)

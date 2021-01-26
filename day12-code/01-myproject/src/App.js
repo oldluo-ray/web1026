1a9d9ba4f8eed8ed7e086211cce8adcf535f2780
oldluo-ray <luodianlei7@163.com>
2021-01-26 16:29:21 +0800

    添加任务

--- day12-code/01-myproject/src/App.js
+++ day12-code/01-myproject/src/App.js
@@ -24,11 +24,30 @@ export default class App extends Component {
     ],
   }
 
+  getTodoName = (todoName) => {
+    //  根据todoName生成一个对象
+    let obj = {
+      id: Date.now(),
+      todoName,
+      isDone: false,
+    }
+
+    let { list } = this.state
+    // 注意: 未来使用的很多第三方的库/框架.对比数据都是浅层对比.随意养成一个好的习惯,不要直接修改复杂数据类型的本身的数据,应该根据原来的数据,创建一份新的数据,然后修改新的数据,最终将新的数据复制给原来存储数据的变量
+    // 根据list,创建一个新的数组
+    let newList = [...list]
+    newList.unshift(obj)
+
+    this.setState({
+      list: newList,
+    })
+  }
+
   render() {
     return (
       <div className="todo-container">
         <div className="todo-wrap">
-          <Header></Header>
+          <Header fn={this.getTodoName}></Header>
           <List list={this.state.list}></List>
           <Footer></Footer>
         </div>
--- day12-code/01-myproject/src/components/Header/Header.js
+++ day12-code/01-myproject/src/components/Header/Header.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 import './Header.css'
 export default class Header extends Component {
+  state = {
+    todoName: '',
+  }
+  handle = (e) => {
+    this.setState({
+      todoName: e.target.value,
+    })
+  }
+
+  // 监听键盘事件的处理函数
+  keyUpHandle = (e) => {
+    // 表示按下回车键
+    if (e.keyCode === 13) {
+      //将用户输入结果,插入到todolist这个数组中
+      this.props.fn(this.state.todoName)
+    }
+  }
   render() {
     return (
       <div className="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认" />
+        <input
+          type="text"
+          placeholder="请输入你的任务名称，按回车键确认"
+          value={this.state.todoName}
+          onChange={this.handle}
+          onKeyUp={this.keyUpHandle}
+        />
       </div>
     )
   }

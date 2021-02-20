e7bff266c79cc1ec65a392d1ded84ed03d8bc417
oldluo-ray <luodianlei7@163.com>
2021-02-20 10:08:37 +0800

    login布局完成

--- react_mobile_1026/src/config/router.js
+++ react_mobile_1026/src/config/router.js
@@ -1,11 +1,13 @@
 // 由于项目中可能有多个页面,手动定义Route组件就不是很方便了,所以定义一个数组,根据数组,动态的渲染Route组件
 import Home from '../pages/Home'
 import Login from '../pages/Login'
+
+// 这个数组最终要渲染出来一个个的Route组件,所以这个数组又被人为的称为路由表
 export default [
   {
     path: '/',
     component: Home,
-    exact: true
+    exact: true,
   },
   {
     path: '/home',
--- react_mobile_1026/src/pages/Login/index.js
+++ react_mobile_1026/src/pages/Login/index.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
-import { NavBar, Icon } from 'antd-mobile'
+import {
+  NavBar,
+  Icon,
+  InputItem,
+  WingBlank,
+  Button,
+  WhiteSpace,
+} from 'antd-mobile'
 
 export default class Login extends Component {
   render() {
@@ -12,6 +19,49 @@ export default class Login extends Component {
         >
           硅谷注册登录
         </NavBar>
+        <WingBlank>
+          <InputItem placeholder="请输入手机号">
+            <div className="inp-phone">
+              <span>+86</span>
+              <Icon type="down" />
+            </div>
+          </InputItem>
+          <div className="inp-phone">
+            <InputItem placeholder="请输入手机验证码"></InputItem>
+            <div className="inp-code-tip">获取验证码</div>
+          </div>
+          <WhiteSpace></WhiteSpace>
+          <WhiteSpace></WhiteSpace>
+          <WhiteSpace></WhiteSpace>
+          <WingBlank>
+            <Button type="warning" disabled className="login-btn">
+              登录
+            </Button>
+            <WhiteSpace></WhiteSpace>
+            <WhiteSpace></WhiteSpace>
+            <div className="login-nav">
+              <div>账号密码登录</div>
+              <div>手机快速注册</div>
+            </div>
+            <div className="login-other">
+              <div className="line"></div>
+              <div className="text">其他登录方式</div>
+              <div className="line"></div>
+            </div>
+            <div className="login-oauth">
+              <i className="iconfont icon-github"></i>
+              <i className="iconfont icon-qq"></i>
+              <i className="iconfont icon-wechat"></i>
+            </div>
+
+            <div className="login-bottom">
+              <span>
+                未注册的手机号验证后将自动创建硅谷账号, 登录即代表您已同意
+                <a href="">硅谷隐私政策</a>
+              </span>
+            </div>
+          </WingBlank>
+        </WingBlank>
       </div>
     )
   }
